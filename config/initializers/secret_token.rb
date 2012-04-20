7e75d7f5cde69b9a4cef9f448d15c24f36755987
Cyril Mougel <cyril.mougel@gmail.com>
2012-04-20 18:19:04 +0200

    start project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pingit::Application.config.secret_token = '7b0abf25bfbfd0290b086f4ba249f3de544ab9f40394f56ed7c40abf2f0ed7ec4afb4ee57ccc3cf2f4e9467eb21fb66dc476a43d5242e7389b90d2579e8623f8'
